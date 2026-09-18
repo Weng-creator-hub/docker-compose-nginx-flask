4d1ef9a0c09e3c49a956f7563e338bcc989be1d1
stevenweng <w1106790367@163.com>
2026-09-18 10:31:07 +0800

    feat: release version 1.1.0

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def health():
 
 @app.route("/version")
 def version():
-    return {"service": "backend-demo", "version": "1.0.0"}, 200
+    return {"service": "backend-demo", "version": "1.1.0"}, 200
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=8080)
